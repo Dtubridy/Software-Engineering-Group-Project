44ba8a284e219194a777f354f98d5336c16a1899
DAVIN O BRIEN( 6-10-2017 12-11 <danu7_od3@danu7>
2018-04-10 01:37:34 +0100

    called api and updated upcoming fixtures page

---
+++ routes/index.js
@@ -0,0 +1,121 @@
+var express = require('express');
+var router = express.Router();
+var Comment = require('../models/comments');
+var jwt = require('jsonwebtoken');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+/* GET feed page. */
+router.get('/feed', function(req, res, next) {
+
+    try {
+        var jwtString = req.cookies.Authorization.split(" ");
+        var profile = verifyJwt(jwtString[1]);
+        if (profile) {
+            res.render('feed');
+        }
+    }catch (err) {
+            res.json({
+                "status": "error",
+                "body": [
+                    "You are not logged in."
+                ]
+            });
+        }
+});
+/* Get about us page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+  });
+  
+  /* Get Leaderboard page */
+  router.get('/leaderboard', function(req, res, next) {
+  res.render('leaderboard', { title: 'Express' });
+});
+
+/* Get LiveScore page */
+  router.get('/liveScore', function(req, res, next) {
+  res.render('liveScore', { title: 'Express' });
+});
+
+/* Get upcoming Matches page */
+  router.get('/upcomingMatches', function(req, res, next) {
+  res.render('upcomingMatches', { title: 'Express' });
+});
+
+router.get('/chat', function(req, res, next) {
+  res.render('chat', { title: 'Express' });
+});
+
+router.get('/support', function(req, res, next) {
+  res.render('support', { title: 'Express' });
+});
+
+/**
+ * Adds comments to our database
+ */
+router.post('/addComment', function(req, res, next) {   
+    // Extract the request body which contains the comments
+    comment = new Comment(req.body);
+    comment.save(function (err, savedComment) {
+        if (err)
+            throw err;
+
+        res.json({
+            "id": savedComment._id
+        });
+    });
+});
+/**
+ * Returns all comments from our database
+ */
+router.get('/getComments', function(req, res, next) {
+
+    Comment.find({}, function (err, comments) {
+        if (err)
+            res.send(err);
+        res.json(comments);
+    });
+});
+/**
+  Updates a comment already in the database
+ */
+router.put('/updateComment/:id', function(req, res, next){
+
+    var id = req.params.id;
+    Comment.update({_id:id}, req.body, function (err) {
+        if (err)
+            res.send(err);
+
+        res.json({status : "Successfully updated the document"});
+    });
+});
+
+/**
+ * Deletes a comment from the database
+ */
+router.delete('/removeComment/:id', function(req, res, next){
+
+    var id = req.params.id;
+    Comment.remove({_id:id}, function (err) {
+        if (err)
+            res.send(err);
+
+        res.json({status : "Successfully removed the document"});
+    });
+});
+
+/*
+ Verifies a JWT
+ */
+function verifyJwt(jwtString) {
+
+    var value = jwt.verify(jwtString, 'CSIsTheWorst');
+    return value;
+}
+
+
+
+module.exports = router;
